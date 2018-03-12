4036486160b854da07d729eb7533dbc2f9067d20
Hayson <heyson@sina.cn>
2018-03-13 00:15:27 +0800

    fix bug and update edit botton

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -9,7 +11,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+
   end
 
   def new
@@ -18,6 +20,8 @@ class GroupsController < ApplicationController
 
 def create
     @group = Group.new(group_params)
+    @group.user = current_user
+
     if @group.save
     redirect_to groups_path
   else
@@ -26,7 +30,7 @@ def create
 end
 
   def update
-    @group = Group.find(params[:id])
+
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -35,7 +39,6 @@ end
   end
 
   def destroy
-    @group = Group.find(params[:id])
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
@@ -49,5 +52,12 @@ end
     params.require(:group).permit(:title, :description)
   end
 
+  def find_group_and_check_permission
+  @group = Group.find(params[:id])
+
+  if current_user != @group.user
+    redirect_to root_path, alert: "You have no permission."
+  end
+end
 
 end
